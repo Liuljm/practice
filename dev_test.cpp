ebe065a63ae8ab87322664c6380169d170f86f42
ljm <320464538@qq.com>
2024-08-12 18:54:47 +0800

    功能一的实现

---
+++ dev_test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+void project(void)
+{
+	
+	cout << "功能一的实现" << endl;
+
+}
+
+
+int main(void)
+{
+	project();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
