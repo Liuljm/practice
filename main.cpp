8b82d87b7b18383033399a081adf27236383de54
ljm <320464538@qq.com>
2024-08-12 16:48:22 +0800

    add num i

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 
-
-
-
-
-
-
-
-
-
-
+int num = 10;
 
 int main(void)
 {
+	int i = 0;
+
 	cout << "hello world!" << endl;
+
+	for(i=0;i<num;i++)
+	{
+		cout << i << endl;
+	}
+
+	
 
 	return 0;
 
